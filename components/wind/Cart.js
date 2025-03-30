729930ccbd50c69030f0ee39185fd7c741fd7050
Rahul kumar <stkwhy000@gmail.com>
2025-03-30 20:00:40 +0530

    addon

--- components/wind/Cart.js
+++ components/wind/Cart.js
@@ -195,7 +195,7 @@ export default function Cart() {
                         <Image
                           src={cartItem.image}
                           alt={cartItem.name || "Product"}
-                          className="w-full h-auto object-cover rounded-lg"
+                          className="w-full h-auto object-contain rounded-lg"
                           radius="md"
                           width="100%"
                           height={180}
