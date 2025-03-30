63a194defe48659d472d2fcf1269229d0ff777d0
Rahul kumar <stkwhy000@gmail.com>
2025-03-30 19:48:00 +0530

    addon

--- components/wind/summary/Summary.jsx
+++ components/wind/summary/Summary.jsx
@@ -169,9 +169,9 @@ const Summary = () => {
               <div className="w-2/3 p-4 flex justify-between items-center">
                 <span>{cartItem.quantity || "Not selected"}</span>
                 <div className="flex items-center">
-                  <span className="font-medium">
+                  {/* <span className="font-medium">
                     ₹ {itemPrice}
-                  </span>
+                  </span> */}
                   <button className="text-[#253670] ml-4" onClick={()=>router.push('/packaging-type')}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                       <path d="M12 20h9"></path>
