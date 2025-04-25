723d03a529f5181accc6b54a99fa92337d335ed0
Rahul kumar <stkwhy000@gmail.com>
2025-04-25 13:30:05 +0530

    product page

--- components/wind/products/Product_details.jsx
+++ components/wind/products/Product_details.jsx
@@ -718,12 +718,9 @@ import ImageComparisonFeature from "../ImageComparisonFeature";
                 
                 {/* Carousel-style Gallery with center-focused layout */}
                 <div className="relative overflow-hidden rounded-xl py-8 ">
-                  <h3 className="text-lg font-semibold text-[#143761] mb-4 flex justify-center items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                    </svg>
-
-                    <h1 className="text-3xl font-bold">This is what your Doypack could look like.</h1>
+                  <h3 className="text-lg font-semibold text-[#000000] mb-4 flex justify-center items-center">
+                    
+                    <h1 className="text-3xl text-center font-bold">This is what your Doypack could look like.</h1>
                   </h3>
                   
                   {/* Main carousel display */}
