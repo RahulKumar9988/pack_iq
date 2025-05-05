9c0d7a9710874c7fc2ca4978ea46d2d091764f41
Rahul kumar <stkwhy000@gmail.com>
2025-05-05 17:11:08 +0530

    product

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,12 @@ const nextConfig = {
         port: "",
         pathname: "/**", // This allows all paths on packiro.com
       },
+      {
+        protocol: "https",
+        hostname: "packiqbackend.setstatetech.com",
+        port: "",
+        pathname: "/**", // This allows all paths on packiro.com
+      },
     ],
   },
 };
