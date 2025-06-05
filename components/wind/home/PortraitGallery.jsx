4f205ef258d7fc3418936d2449cea4eaaa5eb46d
Rahul kumar <stkwhy000@gmail.com>
2025-06-05 23:39:02 +0530

    live rzp

--- components/wind/home/PortraitGallery.jsx
+++ components/wind/home/PortraitGallery.jsx
@@ -98,11 +98,8 @@ export default function LinkedPortraitGallery() {
 
       {/* Loading overlay */}
       {isLoading && (
-        <div className="absolute inset-0 bg-gradient-to-br from-indigo-50 to-white flex items-center justify-center z-50">
-          <div className="relative">
-            <div className="w-16 h-16 border-4 border-blue-200 border-t-blue-600 rounded-full animate-spin"></div>
-            <div className="absolute inset-0 w-16 h-16 border-4 border-transparent border-r-purple-400 rounded-full animate-spin animation-delay-75"></div>
-          </div>
+        <div className="absolute inset-0 flex items-center justify-center z-50">
+          
         </div>
       )}
 
