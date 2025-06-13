8376acb8239af103be9229b6402a87030304f5e9
Rahul kumar <stkwhy000@gmail.com>
2025-06-13 17:46:56 +0530

    ablut us

--- components/wind/home/PortraitGallery.jsx
+++ components/wind/home/PortraitGallery.jsx
@@ -104,7 +104,7 @@ export default function LinkedPortraitGallery() {
       {/* Loading overlay */}
       {isLoading && (
         <div className="absolute inset-0 flex items-center justify-center z-50">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+          {/* <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div> */}
         </div>
       )}
 
